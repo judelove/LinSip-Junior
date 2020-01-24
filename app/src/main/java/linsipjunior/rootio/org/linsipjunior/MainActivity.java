14729423369b8566c38379b906b5658868f92e90
Jude <jude19love@gmail.com>
2020-01-24 23:00:33 +0300

    Added permissions to comply with Android Oreo upwards

--- app/src/main/java/linsipjunior/rootio/org/linsipjunior/MainActivity.java
+++ app/src/main/java/linsipjunior/rootio/org/linsipjunior/MainActivity.java
@@ -1,15 +1,23 @@
 package linsipjunior.rootio.org.linsipjunior;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.ComponentName;
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -23,6 +31,7 @@ import android.widget.Toast;
 
 import org.linphone.core.Call;
 import org.linphone.core.RegistrationState;
+import org.linphone.core.Utils;
 
 import java.util.Calendar;
 
@@ -30,6 +39,7 @@ public class MainActivity extends AppCompatActivity implements SipEventsNotifiab
     private ComponentName registrationService;
     private SIPHandler handler;
     private Call call;
+    private String[] requirePermissions = new String[]{Manifest.permission.CALL_PHONE, Manifest.permission.RECORD_AUDIO,  Manifest.permission.CAMERA, Manifest.permission.PROCESS_OUTGOING_CALLS,  Manifest.permission.READ_CONTACTS, Manifest.permission.WRITE_CONTACTS,  Manifest.permission.READ_EXTERNAL_STORAGE};
 
 
     public void toggleRegistration(View v) {
@@ -46,15 +56,60 @@ public class MainActivity extends AppCompatActivity implements SipEventsNotifiab
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        this.registrationService = this.startService(new Intent(this, SIPHandler.class));
+        for (String permission : requirePermissions) {
+            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                askAllPermissions();
+            } else {
+
+                this.registrationService = this.startService(new Intent(this, SIPHandler.class));
+            }
+        }
     }
 
 
     @Override
     protected void onStart() {
         super.onStart();
+
         this.bindService(new Intent(this, SIPHandler.class), new ServiceConnectionAgent(), Context.BIND_AUTO_CREATE);
 
+    }
+
+    @Override
+    protected void onResume() {
+
+        super.onResume();
+        this.bindService(new Intent(this, SIPHandler.class), new ServiceConnectionAgent(), Context.BIND_AUTO_CREATE);
+    }
+
+    private void askAllPermissions() {
+        ActivityCompat.requestPermissions(this, requirePermissions, 1);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        boolean allGranted = true;
+        for (int permissionIndex : grantResults) {
+            if (permissionIndex != PackageManager.PERMISSION_GRANTED) {
+                allGranted = false;
+            }
+        }
+        if (!allGranted) {
+            warnOnScreen(this, "please go to settings and grant this app the required permissions.", new DialogInterface.OnDismissListener() {
+                @Override
+                public void onDismiss(DialogInterface dialog) {
+                    finish();
+                }
+            });
+        }
+        else
+        {
+            this.bindService(new Intent(this, SIPHandler.class), new ServiceConnectionAgent(), Context.BIND_AUTO_CREATE);
+        }
+    }
+
+    public void warnOnScreen(Activity triggerActivity, String message, DialogInterface.OnDismissListener listener) {
+        new AlertDialog.Builder(triggerActivity).setIcon(R.drawable.attention).setOnDismissListener(listener).setTitle("Warning").setMessage(message).setNeutralButton("Close", null).show();
     }
 
     public void call(View v) {
--- app/src/main/java/linsipjunior/rootio/org/linsipjunior/SIPHandler.java
+++ app/src/main/java/linsipjunior/rootio/org/linsipjunior/SIPHandler.java
@@ -1,15 +1,19 @@
 package linsipjunior.rootio.org.linsipjunior;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.Service;
-import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Binder;
+import android.os.Build;
 import android.os.IBinder;
 import android.support.v4.app.NotificationCompat;
-import android.util.Log;
 import android.widget.Toast;
 
 import org.linphone.core.Address;
@@ -22,14 +26,6 @@ import org.linphone.core.NatPolicy;
 import org.linphone.core.ProxyConfig;
 import org.linphone.core.RegistrationState;
 import org.linphone.core.Transports;
-
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
 
 /**
  * Created by Jude Mukundane on 06/03/2018.
@@ -64,9 +60,32 @@ public class SIPHandler extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        this.startForeground(1, new NotificationCompat.Builder(this).setContentTitle("LinSipJunior").setContentText("LinSipJunior Running").setAutoCancel(false).build());
+        this.startForeground(1, Build.VERSION.SDK_INT < Build.VERSION_CODES.O? new NotificationCompat.Builder(this).setContentTitle("LinSipJunior").setContentText("LinSip Jr. is Running").setAutoCancel(false).build():getNotification());
         return Service.START_STICKY;
 
+    }
+
+
+    @TargetApi(Build.VERSION_CODES.O)
+    private Notification getNotification()
+    {
+    String NOTIFICATION_CHANNEL_ID = "com.example.simpleapp";
+    String channelName = "My Background Service";
+    NotificationChannel chan = new NotificationChannel(NOTIFICATION_CHANNEL_ID, channelName, NotificationManager.IMPORTANCE_NONE);
+    chan.setLightColor(Color.BLUE);
+    chan.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
+    NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+    assert manager != null;
+    manager.createNotificationChannel(chan);
+
+    NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID);
+    Notification notification = notificationBuilder.setOngoing(true)
+            .setSmallIcon(R.mipmap.logo_sj)
+            .setContentTitle("LinSip jr. is running")
+            .setPriority(NotificationManager.IMPORTANCE_MIN)
+            .setCategory(Notification.CATEGORY_SERVICE)
+            .build();
+    return notification;
     }
 
 
@@ -140,6 +159,7 @@ public class SIPHandler extends Service {
     }
 
     void register() {
+        initializeStack();
         this.linphoneCore.removeListener(coreListener);
         new Thread(new Runnable() {
             @Override
